6c684f2f88cad8fbda179c9c4c69288c9e6f5660
aiwaverse <aiwavision@protonmail.com>
2020-10-07 09:00:40 -0300

    more changes to grading_assist

--- grading_assist.py
+++ grading_assist.py
@@ -25,15 +25,21 @@ files_to_delete = [
 ]
 os.chdir(racket_folder)
 students_folders = glob("**", recursive=False)
-students_folders = [
-    "/home/maya/Documents/aiwita/codes/racket/correcao/LISTA6/ANDREI POCHMANN KOENICH_14029701_assignsubmission_file_"
-]
+
 #%%
 for student_folder in students_folders:
     os.chdir(os.path.join(os.getcwd(), student_folder, ""))
     for file_to_copy in files_to_copy:
         shutil.copy(os.path.join(script_folder, file_to_copy), os.getcwd())
-    student_file = glob("listaCap10*")[0]
+    possible_files = glob("listaCap10*")
+    if not possible_files:
+        with open("message.txt", "w") as err_file:
+            err_file.write("O aluno é burro pra caralho e não nomeou o arquivo certo.")
+        continue
+    student_file = possible_files[0]
+    if " " in student_file:
+        os.rename(student_file, student_file.replace(" ", ""))
+        student_file = student_file.replace(" ", "")
     student_file_no_ext = student_file.replace(".rkt", "")
     subprocess.run(["sh", "insertTests.sh", student_file, "testesLista6Exc67.rkt"])
     subprocess.run(
