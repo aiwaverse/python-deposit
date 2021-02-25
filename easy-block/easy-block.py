7b1ca8cd369c44ee46838691a116edb48f0795f4
aiwaverse <aiwavision@protonmail.com>
2021-02-25 16:13:30 -0300

    refactored code, it was really ugly

--- easy-block/easy-block.py
+++ easy-block/easy-block.py
@@ -1,55 +1,99 @@
 #%%
+from typing import Set, Tuple, Union
 import tweepy
 import os
 from dotenv import load_dotenv
 from tqdm import tqdm
+import sys
+
 #%%
-load_dotenv()
-CONSUMER_KEY = os.getenv('TWITTER_API_KEY')
-CONSUMER_SECRET = os.getenv('TWITTER_API_SECRET')
+def get_keys() -> Tuple[Union[str, None], Union[str, None]]:
+    load_dotenv()
+    key = os.getenv("TWITTER_API_KEY")
+    secret = os.getenv("TWITTER_API_SECRET")
+    return (key, secret)
 
-if(CONSUMER_KEY and CONSUMER_SECRET):
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, callback="oob")
 
+def authenticate_user(key: str, secret: str) -> tweepy.API:
+    auth = tweepy.OAuthHandler(key, secret, callback="oob")
     auth_url = auth.get_authorization_url()
     print("Authorization URL: " + auth_url)
-
     verifier = input("PIN: ").strip()
+    if not verifier:
+        raise IOError("Incorret PIN format!")
     auth.get_access_token(verifier)
-
     auth.set_access_token(auth.access_token, auth.access_token_secret)
+    return tweepy.API(auth)
 
-    api = tweepy.API(auth)
-    own_user_id = api.me().id
-    user_friends = set(api.friends_ids(own_user_id))
-    user_followers = set(api.followers_ids(own_user_id))
-    answer = "y"
-    while answer == "y":
-        master_block = input("Type in the @ of the account you want to block!\n")
-        if not master_block:
-            raise IOError("No valid input!")
-        #%%
+
+def get_user_info() -> Tuple[Set[str], Set[str], Set[str]]:
+    user_friends = set(api.friends_ids())
+    user_followers = set(api.followers_ids())
+    user_blocks = set(api.blocks_ids())
+    return (user_friends, user_followers, user_blocks)
+
+
+def get_blocking_information(user_masterblock: str) -> Union[None, Set[str]]:
+    try:
+        followers = set(api.followers_ids(user_masterblock))
+    except tweepy.TweepError:
+        print("User unreachable!")
+        return None
+    else:
+        followers.add(user_masterblock)
+        return followers
+
+
+def generate_blocking_list(user_masterblock: str) -> Union[None, Set[str]]:
+    friends, followers, blocks = get_user_info()
+    blocklist = get_blocking_information(user_masterblock)
+    if blocklist:
+        return blocklist - friends - followers - blocks
+    else:
+        return None
+
+
+def block_users(blocklist: Set[str]) -> None:
+    for user in tqdm(blocklist):
         try:
-            followers = set(api.followers_ids(master_block))
+            api.create_block(user)
         except tweepy.TweepError:
-            print("User unreachable!")
             continue
-        current_blocking = set(api.blocks_ids())
-        blockable = followers - current_blocking - user_friends - user_followers
-        print(f"Now blocking {master_block}")
-        for each_blockable_user in tqdm(blockable):
-            try:
-                api.create_block(each_blockable_user)
-            except tweepy.TweepError:
-                continue
-        api.create_block(master_block)
-        answer_ok = False
-        while not answer_ok:
-            answer = input("Do you wish to block another user and their followers? (y/n)\n").strip().lower()
-            answer_ok = answer in "yn"
-            if not answer_ok:
-                print("Please input a correct answer.")
 
-    # %%
-else:
-    print("Keys not present.")
+
+def do_again() -> bool:
+    answer_ok = False
+    answer = ""
+    while not answer_ok:
+        answer = (
+            input("Do you wish to block another user and their followers? (y/n)\n")
+            .strip()
+            .lower()
+        )
+        answer_ok = answer in "yn"
+        if not answer_ok:
+            print("Please input a correct answer.")
+    if answer == "y":
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    CONSUMER_KEY, CONSUMER_SECRET = get_keys()
+    if not CONSUMER_KEY or not CONSUMER_SECRET:
+        raise ValueError("Keys not found on .env!")
+    api = authenticate_user(CONSUMER_KEY, CONSUMER_SECRET)
+    keep_going = True
+    while keep_going:
+        to_block = input("Type in the @ of the account you want to block!\n").strip()
+        if not to_block:
+            raise ValueError("Empty input, please try again.")
+        blocklist = generate_blocking_list(to_block)
+        if not blocklist:
+            raise ValueError(
+                "Blocklist couldn't be generated, check if the @ is correct"
+            )
+        block_users(blocklist)
+        keep_going = do_again()
+
